6d8530c1475f09c264b0f80f03b9ea0ccfade575
cobateknologi <mikhsannuryaman@gmail.com>
2023-12-06 15:33:25 +0700

    menambahkan  fitur kirim pesan

--- src/components/sections/writeMsg/writeMsg.jsx
+++ src/components/sections/writeMsg/writeMsg.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import InputBox from "./inputBox";
+import Safe from "react-safe";
 
 export default function WriteMsg() {
   const [msg, setMsg] = useState({
@@ -23,13 +24,18 @@ export default function WriteMsg() {
       <p className="text-textSecondary mt-2" data-aos="fade-up" data-aos-duration="1600" data-aos-delay="600">
         Berikan Ucapan Terbaik Untuk Kedua Mempelai
       </p>
-      <form className="mt-8 space-y-4" onChange={(e) => writeMessage(e)}>
-        <InputBox placeholder={"Nama"} name={"nama"} />
-        <InputBox placeholder={"Masukan Ucapan..."} name={"message"} />
-      </form>
-      <button onClick={sendMessage} className="block px-7 py-2 mx-auto bg-[#6D4B64] text-white rounded-lg mt-8">
-        Kirim
-      </button>
+
+      <div id="disqus_thread"></div>
+      <Safe.script>
+        {(function () {
+          // DON'T EDIT BELOW THIS LINE
+          var d = document,
+            s = d.createElement("script");
+          s.src = "https://https-ipan-vera-wedding-vercel-app.disqus.com/embed.js";
+          s.setAttribute("data-timestamp", +new Date());
+          (d.head || d.body).appendChild(s);
+        })()}
+      </Safe.script>
     </section>
   );
 }
